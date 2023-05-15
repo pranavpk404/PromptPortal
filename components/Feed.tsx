239febd4466949f9e13fbed1da1d18d0b73bb352
Pranav <pranavpk404@gmail.com>
2023-05-15 19:22:11 +0530

    added search

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -4,13 +4,13 @@ import { useState, useEffect } from "react";
 
 import PromptCard from "./PromptCard";
 
-export const PromptCardList = ({ data, handleTagClick }) => {
+const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className="mt-16 prompt_layout">
-      {data.map((prompt) => (
+      {data.map((post) => (
         <PromptCard
-          key={prompt._id}
-          post={prompt}
+          key={post._id}
+          post={post}
           handleTagClick={handleTagClick}
         />
       ))}
@@ -20,21 +20,53 @@ export const PromptCardList = ({ data, handleTagClick }) => {
 
 const Feed = () => {
   const [allPosts, setAllPosts] = useState([]);
+
   // Search states
   const [searchText, setSearchText] = useState("");
   const [searchTimeout, setSearchTimeout] = useState(null);
   const [searchedResults, setSearchedResults] = useState([]);
+
+  const fetchPosts = async () => {
+    const response = await fetch("/api/prompt");
+    const data = await response.json();
+
+    setAllPosts(data);
+  };
+
   useEffect(() => {
-    const fetchPrompts = async () => {
-      const res = await fetch("/api/prompt");
-      const data = await res.json();
-      setAllPosts(data);
-    };
-    fetchPrompts();
+    fetchPosts();
   }, []);
 
-  const handleSearchChange = () => {};
-  const handleTagClick = () => {};
+  const filterPrompts = (searchtext) => {
+    const regex = new RegExp(searchtext, "i"); // 'i' flag for case-insensitive search
+    return allPosts.filter(
+      (item) =>
+        regex.test(item.creator.username) ||
+        regex.test(item.tag) ||
+        regex.test(item.prompt)
+    );
+  };
+
+  const handleSearchChange = (e) => {
+    clearTimeout(searchTimeout);
+    setSearchText(e.target.value);
+
+    // debounce method
+    setSearchTimeout(
+      setTimeout(() => {
+        const searchResult = filterPrompts(e.target.value);
+        setSearchedResults(searchResult);
+      }, 500)
+    );
+  };
+
+  const handleTagClick = (tagName) => {
+    setSearchText(tagName);
+
+    const searchResult = filterPrompts(tagName);
+    setSearchedResults(searchResult);
+  };
+
   return (
     <section className="feed">
       <form className="relative w-full flex-center">
